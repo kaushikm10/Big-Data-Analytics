ce41fe6b458f347ae39af91abccfc0367f9d1dbf
kaushikm10 <kaushik.metha@somaiya.edu>
2023-04-17 02:19:55 -0600

    model integrated

--- app.py
+++ app.py
@@ -3,17 +3,24 @@ import os
 import psycopg2
 import requests
 from polygon import RESTClient
-import datetime
 import pickle
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
-
-
+import pickle
+from tensorflow.keras.models import load_model
+import numpy as np
+import sklearn
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
 api_key = "Xljq9nB1MP3h8PhC5ZIPjp9QyjI1t11N"
 client = RESTClient(api_key=api_key)
+
+tickers = pickle.load(open("model/tickers.pkl", "rb"))
+tickers_map = {ticker: i for i, ticker in enumerate(tickers)}
+model = load_model("model/prediction_model.h5")
+scalers = pickle.load(open("model/scalers.pkl", "rb"))
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -26,9 +33,12 @@ def home():
     data = []
     ema = []
     symbol = "STOCK"
+    prediction = None
     if request.method == "POST":
         symbol = request.form['stock']
-        bars = client.get_aggs(ticker=symbol, multiplier=1, timespan="day", from_="1970-01-01", to="2023-04-05") 
+        start = (datetime.today() - timedelta(days=2*365)).strftime('%Y-%m-%d')
+        end= datetime.today().strftime('%Y-%m-%d')
+        bars = client.get_aggs(ticker=symbol, multiplier=1, timespan="day", from_=start, to=end) 
         for i, bar in enumerate(bars): 
             temp = datetime.fromtimestamp(int(str(bar.timestamp)[:10]))
             date.append(f"{temp.year}-{temp.month}-{temp.day}")
@@ -37,10 +47,15 @@ def home():
             low.append(bar.low)
             open.append(bar.open)
             data.append([bar.timestamp, bar.open, bar.high, bar.low, bar.close])
+        pred_data = np.expand_dims(np.array(close[-60:]), 0)
+        scaler = scalers[symbol]
+        prediction = scaler.inverse_transform(model.predict(pred_data)[tickers_map[symbol]])[0][0]
+        print(prediction)
+
         ema = list(pd.Series(close).ewm(span=10, adjust=False).mean().values)
 
         hidden = 0
-    return render_template('home.html', ema=ema, close=close, open=open, low=low, high=high, date=date, data=data, hidden=hidden, symbol=symbol)
+    return render_template('home.html', ema=ema, close=close, open=open, low=low, high=high, date=date, data=data, hidden=hidden, symbol=symbol, tickers=tickers, prediction=prediction)
 
 
 if __name__ == '__main__':
