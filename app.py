22ea73a84e5a74f1e80d250f33407e7a7df7bc24
kaushikm10 <kaushik.metha@somaiya.edu>
2023-05-02 02:38:43 -0600

    login-signup-added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, url_for, request
+from flask import Flask, jsonify, render_template, url_for, request, session, redirect
 import os
 import psycopg2
 import requests
@@ -12,7 +12,7 @@ import sklearn
 from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
-
+app.secret_key = 'big-data-analytics'
 api_key = "Xljq9nB1MP3h8PhC5ZIPjp9QyjI1t11N"
 client = RESTClient(api_key=api_key)
 
@@ -33,8 +33,11 @@ try:
 except:
     print("Not Connected")
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/stock/', methods=['GET', 'POST'])
 def home():
+
+    if session['logged_in'] is False:
+        return redirect(url_for('index'))
     date = []
     close = []
     high = []
@@ -48,18 +51,19 @@ def home():
     if request.method == "POST":
         symbol = request.form['stock']
         start = (datetime.today() - timedelta(days=2*365)).strftime('%Y-%m-%d')
-        end= datetime.today().strftime('%Y-%m-%d')
+        end = datetime.today().strftime('%Y-%m-%d')
         bars = client.get_aggs(ticker=symbol, multiplier=1, timespan="day", from_=start, to=end) 
         for i, bar in enumerate(bars): 
             temp = datetime.fromtimestamp(int(str(bar.timestamp)[:10]))
-            date.append(f"{temp.year}-{temp.month}-{temp.day}")
+            d = "{}-{}-{}".format(temp.year, temp.month, temp.day)
+            date.append(d)
             close.append(bar.close)
             high.append(bar.high)
             low.append(bar.low)
             open.append(bar.open)
             data.append([bar.timestamp, bar.open, bar.high, bar.low, bar.close])
         last_date = datetime.strptime(end, '%Y-%m-%d')
-        query = f"SELECT price FROM stock_data WHERE date_predicted = '{last_date}' AND stock_symbol = '{symbol}';"
+        query = "SELECT price FROM stock_data WHERE date_predicted = '{}' AND stock_symbol = '{}';".format(last_date, symbol)
         cursor.execute(query)
         results = cursor.fetchall()
 
@@ -70,6 +74,60 @@ def home():
         hidden = 0
     return render_template('home.html', ema=ema, close=close, open=open, low=low, high=high, date=date, data=data, hidden=hidden, symbol=symbol, tickers=tickers, prediction=prediction)
 
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            print(username, password, email)
+            query = "SELECT * FROM USERS WHERE USERNAME_='{}';".format(username)
+            cursor.execute(query)
+            res = cursor.fetchall()
+            print(res)
+            if len(res) != 0:
+                return redirect(url_for('index'))                
+            else:
+                query = "INSERT INTO USERS (USERNAME_, PASSWORD_, EMAIL_) VALUES (%s, %s, %s)"
+                values = (username, password, email)
+                cursor.execute(query, values)
+                conn.commit()
+                return redirect(url_for('login'))
+        except Exception as e:
+            print(e)
+            return render_template('index.html', message="User Already Exists")
+    else:
+        return render_template('register.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        u = request.form['username']
+        p = request.form['password']
+        query = "SELECT * FROM USERS WHERE USERNAME_='{}';".format(u)
+        cursor.execute(query)
+        res = cursor.fetchone()
+        if res:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        return render_template('index.html', message="Incorrect Details")
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    session['logged_in'] = False
+    if request.method=='POST':
+        req_type = request.form['type']
+        if req_type == 'login':
+            return render_template('login.html')
+        else:
+            return render_template('register.html')
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
